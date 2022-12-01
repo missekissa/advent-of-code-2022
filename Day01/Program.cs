63f29637b239564b27f8955134dcdf87320ff476
JP <83371069+missekissa@users.noreply.github.com>
2022-12-01 19:49:42 +0200

    Solve day 1 and improve the solution

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -14,20 +14,18 @@ namespace Day01
 
             string seperator = Environment.NewLine + Environment.NewLine;
 
-            var data = input.ParseInput(seperator).Select(x => x.Split(Environment.NewLine).Select(int.Parse).ToList()).ToList();
-
-            int maksimi = Part1(data);
-            Console.WriteLine(maksimi);
-            
+            var data = input.ParseInput(seperator).Select(x => x.Split(Environment.NewLine)
+                .Select(int.Parse).ToList()).ToList().Select(x => x.Sum()).ToList();
+                 
+            Console.WriteLine(Part1(data));
+            Console.WriteLine(Part2(data));            
         }
 
-        //private static int Part1(List<List<int>> calories)
-        //{
-        //    return calories.Select(x => x.Sum()).Max();
-        //}
+        private static int Part1(List<int> sums) =>
+            sums.Max();
 
-        private static int Part1(List<List<int>> calories) =>
-          calories.Select(x => x.Sum()).Max();
+        private static int Part2(List<int> sums) =>
+            sums.OrderByDescending(x => x).Take(3).Sum();
 
     }
 }
