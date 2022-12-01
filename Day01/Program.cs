e0c1eac4d1dfd79270dbbdf105bad0c73814e800
JP <83371069+missekissa@users.noreply.github.com>
2022-12-01 19:36:17 +0200

    Write helper methods and solve first part

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Day01
+﻿using Lib;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day01
 {
-    internal class Program
+    public class Program
     {
+                                   
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "Input.txt";
+            string input = path.ReadInput();
+
+            string seperator = Environment.NewLine + Environment.NewLine;
+
+            var data = input.ParseInput(seperator).Select(x => x.Split(Environment.NewLine).Select(int.Parse).ToList()).ToList();
+
+            int maksimi = Part1(data);
+            Console.WriteLine(maksimi);
+            
         }
+
+        //private static int Part1(List<List<int>> calories)
+        //{
+        //    return calories.Select(x => x.Sum()).Max();
+        //}
+
+        private static int Part1(List<List<int>> calories) =>
+          calories.Select(x => x.Sum()).Max();
+
     }
 }
---
+++ Lib/Extensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Lib
+{
+    public static class Extensions
+    {
+        public static string ReadInput(this string fileLocation) =>
+            File.ReadAllText(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, fileLocation));
+
+        public static IEnumerable<string> ParseInput(this string input, string seperator) =>
+            input.Split(seperator, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+    }
+}
