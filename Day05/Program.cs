6da60dc00b8856e2c695737342c3b9f2bd91f4dc
JP <83371069+missekissa@users.noreply.github.com>
2022-12-05 20:35:42 +0200

    Create solution for day 5

---
+++ Day05/Program.cs
@@ -0,0 +1,24 @@
+﻿using Day05;
+using Lib;
+using System.Text.RegularExpressions;
+
+//string path = "Input.txt";
+string path = "Example.txt";
+
+List<string> input = path.ReadInput().ParseInput(Environment.NewLine + Environment.NewLine).ToList();
+
+//Parse stacks
+Regex lastNumber = new(@"\d$");
+int numberOfStacks = int.Parse(lastNumber.Match(input[0]).Value);
+
+var test = input[0].Split(Environment.NewLine);
+
+
+Console.WriteLine(test);
+
+//Parse steps
+Queue<Step> steps = new();
+input[1].Split(Environment.NewLine).ToList().ForEach(line => steps.Enqueue(new Step(line)));
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(numberOfStacks);
---
+++ Day05/Step.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day05
+{
+    public class Step
+    {
+        public Step(int move, int from, int to)
+        {
+            Move = move;
+            From = from;
+            To = to;
+        }
+
+        public Step(string line)
+        {
+            var matches = number.Matches(line).Select(x => int.Parse(x.Value)).ToArray();
+            Move = matches[0];
+            From = matches[1];
+            To = matches[2];           
+        }
+
+        int Move { get; set; }
+        int From { get; set; }
+        int To { get; set; }
+
+        static readonly Regex number = new Regex(@"\d+");
+
+        public override string? ToString()
+        {
+            return $"move {Move} from {From} to {To}";
+        }
+    }
+}
